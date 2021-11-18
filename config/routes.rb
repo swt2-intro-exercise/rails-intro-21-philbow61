8bb01a34f7bf9f03560e622829155e5ba9f11eea
philipraetsch <janphilip.raetsch@student.hpi.uni-potsdam.de>
2021-11-19 00:28:07 +0100

    2

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  resources :authors
  
    
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  
+  resources :authors
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
