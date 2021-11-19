1d3752c7bedec52790dedf23ff04a2db90174168
philipraetsch <janphilip.raetsch@student.hpi.uni-potsdam.de>
2021-11-19 14:59:17 +0100

    5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,19 @@
 class AuthorsController < ApplicationController
+    
     def new 
+        @author= Author.new
+    end
+
+    def create 
+        @author = Author.new(authors_params)
+        if @author.save
+            redirect_to root_path, notice: 'Success!'
+        else
+            render "New"
+        end 
+    end
+    private 
+        def authors_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,17 @@ describe "New author page", type: :feature do
       expect(page).to have_field('author[last_name]')
       expect(page).to have_field('author[homepage]')
    end
-end
+
+   it "should make a database entry based on the inputs" do
+      visit new_author_path
+
+      page.fill_in 'author[first_name]', with: "Alan"
+      page.fill_in 'author[last_name]', with: "Turing"
+      page.fill_in 'author[homepage]', with: "http://wikipedia.org/Alan_Turing"
+
+      find('input[type="submit"]').click
+
+      expect(Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").blank?).to be false
+
+   end
+end 
