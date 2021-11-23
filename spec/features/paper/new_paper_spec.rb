8cad171f04737cbb7d747c383661705965081016
philipraetsch <janphilip.raetsch@student.hpi.uni-potsdam.de>
2021-11-23 13:27:44 +0100

    14

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -4,5 +4,10 @@ describe "New paper page", type: :feature do
     it "should reject a paper with no title" do 
         @paper = Paper.new(title:"")
         expect(@paper).to_not be_valid
-     end
+    end
+
+    it "should have and belong to many authors" do
+        @paper = Paper.new
+        expect(@paper.authors).to be_empty
+    end
 end
